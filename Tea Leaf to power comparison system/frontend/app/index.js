98fc8e4064149040bd3b995482816124d175fe9d
Samadhi Hewage <nipunikahewage71@gmail.com>
2025-12-23 06:21:41 +0530

    Update index.js

--- Tea Leaf to power comparison system/frontend/app/index.js
+++ Tea Leaf to power comparison system/frontend/app/index.js
@@ -1,26 +1,33 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
+import Login from "./login";
 
 export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ fontSize: 28, marginBottom: 20 }}>Tea Bi</Text>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Welcome Back!</Text>
+  return <Login />;
+}
 
-      <Link
-        href="/Dashboard"
-        style={{
-          backgroundColor: "#10b981",
-          padding: 10,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ color: "white", fontWeight: "bold" }}>Dashboard</Text>
-      </Link>
+//export default function Index() {
+ // return (
+   // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      //  <Text style={{ fontSize: 28, marginBottom: 20 }}>Tea Bi</Text>
+     // <Text style={{ fontSize: 24, marginBottom: 20 }}>Welcome Back!</Text>
+
+     // <Link
+       // href="/Dashboard"
+       // style={{
+        //  backgroundColor: "#10b981",
+        //  padding: 10,
+         // borderRadius: 8,
+       // }}
+    //  >
+       // <Text style={{ color: "white", fontWeight: "bold" }}>Dashboard</Text>
+     // </Link>
 
     
       
-    </View>
-  );
-}
+   // </View>
+ // );
+
+  
+//}
